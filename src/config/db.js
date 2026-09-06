fcbfaedc5d4d04bb8e4abb4ecda9ca9c28cb8c4c
Bishwash3 <bhusalbiswash2@gmail.com>
2026-09-06 13:36:27 +0545

    setup express, database and file structured

---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URL}/${process.env.DB_NAME}`)
+        console.log(`MongoDB connected: ${connectionInstance.connection[0].host}`)
+    } catch (error) {
+        console.log("MONGODB CONNECTION FAILED!: ", error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
